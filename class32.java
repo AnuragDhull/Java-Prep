d194e17c6b18f4c3d592fdba865cb3993354cbe1
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2024-04-07 16:40:57 +0530

    Add files via upload

---
+++ class32.java
@@ -0,0 +1,18 @@
+public class class32 {
+
+    static int sum(int x, int y) {
+        int z = x + y;
+        return z;
+
+    }
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 5;
+        int c = sum(a, b);
+
+        System.out.println(c);
+
+    }
+}
---
+++ class33.java
@@ -0,0 +1,16 @@
+public class class33 {
+
+    int sum(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        class32 obj = new class32();
+        int c = obj.sum(a, b);
+        System.out.println(c);
+
+    }
+}
